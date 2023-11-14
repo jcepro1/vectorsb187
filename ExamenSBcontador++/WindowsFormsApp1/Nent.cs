1a64d710b9c6ea008bc4fe491ccdc99ea48675c7
jcepro1 <115606134+jcepro1@users.noreply.github.com>
2023-11-14 08:08:00 -0400

    Add files via upload

---
+++ ExamenSBcontador++/WindowsFormsApp1/Nent.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Nent
+    {
+        private int n;
+        public Nent()
+        {
+            n = 0;
+        }
+
+        public void cargar(int dato)
+        {
+            n = dato;
+        }
+
+        public int descargar()
+        {
+            return n;
+        }
+
+        
+        
+            public bool Veri_Primo()
+            {
+                int r, i;
+                i = 1;
+                if (n > 1)
+                {
+                    do
+                    {
+                        i = i + 1;
+                        r = n % i;
+                    }
+                    while (!(r == 0));
+                }
+                else
+                    i = -1;
+
+                return n == i;
+            }
+
+        // Verificar si un NE pertenece a la serie de Fibonacci /////////////////////////////////////
+        // n=7 => true
+
+        public bool Veri_fibo()
+        {
+            int a = -1;
+            int b = 1;
+            int c;
+            do
+            {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+            while (!((c == n) || (c > n)));
+            return c == n;
+        }
+
+        // Factorial
+
+        public int Factorial()
+            {
+                int f;
+                f = 1;
+                if (n > 0)
+                {
+                    for (var i = n; i >= 1; i += -1)
+                        f = f * i;
+                }
+                else
+                    f = f;
+
+                return f;
+            }
+
+            // Verificar si un NE es Par
+
+            public bool Veri_Par()
+            {
+                return n % 2 == 0;
+            }
+
+            // Verificar si un NE es Impar
+            public bool Veri_Impar()
+            {
+                return n % 2 == 1;
+            }
+
+    }
+
+
+}
