ec4a395e6213ad043de0803344e3646e3408f490
jcepro1 <115606134+jcepro1@users.noreply.github.com>
2023-11-25 12:00:00 -0400

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.opBasicasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mENU1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cARGARToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cARGARRNDToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dESCARGARToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mENU2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cARGARToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.cARGARRBDToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dESCARGARToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.mENU3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cARGARToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.cARGARRNDToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.dESCARGARToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.eJERCICIO1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eJERCICIO2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(132, 62);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 31);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(277, 62);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 31);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(409, 62);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 31);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(132, 132);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(733, 31);
+            this.textBox5.TabIndex = 4;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox6.Location = new System.Drawing.Point(132, 179);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(733, 31);
+            this.textBox6.TabIndex = 5;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox7.Location = new System.Drawing.Point(132, 226);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(733, 31);
+            this.textBox7.TabIndex = 6;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox8.Location = new System.Drawing.Point(132, 273);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(733, 31);
+            this.textBox8.TabIndex = 7;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.opBasicasToolStripMenuItem,
+            this.mENU1ToolStripMenuItem,
+            this.mENU2ToolStripMenuItem,
+            this.mENU3ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(889, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // opBasicasToolStripMenuItem
+            // 
+            this.opBasicasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.eJERCICIO1ToolStripMenuItem,
+            this.eJERCICIO2ToolStripMenuItem});
+            this.opBasicasToolStripMenuItem.Name = "opBasicasToolStripMenuItem";
+            this.opBasicasToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.opBasicasToolStripMenuItem.Text = "EXAMEN";
+            // 
+            // mENU1ToolStripMenuItem
+            // 
+            this.mENU1ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cARGARToolStripMenuItem,
+            this.cARGARRNDToolStripMenuItem,
+            this.dESCARGARToolStripMenuItem});
+            this.mENU1ToolStripMenuItem.Name = "mENU1ToolStripMenuItem";
+            this.mENU1ToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.mENU1ToolStripMenuItem.Text = "MENU 1";
+            // 
+            // cARGARToolStripMenuItem
+            // 
+            this.cARGARToolStripMenuItem.Name = "cARGARToolStripMenuItem";
+            this.cARGARToolStripMenuItem.Size = new System.Drawing.Size(147, 22);
+            this.cARGARToolStripMenuItem.Text = "CARGAR";
+            this.cARGARToolStripMenuItem.Click += new System.EventHandler(this.cARGARToolStripMenuItem_Click);
+            // 
+            // cARGARRNDToolStripMenuItem
+            // 
+            this.cARGARRNDToolStripMenuItem.Name = "cARGARRNDToolStripMenuItem";
+            this.cARGARRNDToolStripMenuItem.Size = new System.Drawing.Size(147, 22);
+            this.cARGARRNDToolStripMenuItem.Text = "CARGAR RND";
+            this.cARGARRNDToolStripMenuItem.Click += new System.EventHandler(this.cARGARRNDToolStripMenuItem_Click);
+            // 
+            // dESCARGARToolStripMenuItem
+            // 
+            this.dESCARGARToolStripMenuItem.Name = "dESCARGARToolStripMenuItem";
+            this.dESCARGARToolStripMenuItem.Size = new System.Drawing.Size(147, 22);
+            this.dESCARGARToolStripMenuItem.Text = "DESCARGAR";
+            this.dESCARGARToolStripMenuItem.Click += new System.EventHandler(this.dESCARGARToolStripMenuItem_Click);
+            // 
+            // mENU2ToolStripMenuItem
+            // 
+            this.mENU2ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cARGARToolStripMenuItem1,
+            this.cARGARRBDToolStripMenuItem,
+            this.dESCARGARToolStripMenuItem1});
+            this.mENU2ToolStripMenuItem.Name = "mENU2ToolStripMenuItem";
+            this.mENU2ToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.mENU2ToolStripMenuItem.Text = "MENU 2";
+            // 
+            // cARGARToolStripMenuItem1
+            // 
+            this.cARGARToolStripMenuItem1.Name = "cARGARToolStripMenuItem1";
+            this.cARGARToolStripMenuItem1.Size = new System.Drawing.Size(147, 22);
+            this.cARGARToolStripMenuItem1.Text = "CARGAR";
+            this.cARGARToolStripMenuItem1.Click += new System.EventHandler(this.cARGARToolStripMenuItem1_Click);
+            // 
+            // cARGARRBDToolStripMenuItem
+            // 
+            this.cARGARRBDToolStripMenuItem.Name = "cARGARRBDToolStripMenuItem";
+            this.cARGARRBDToolStripMenuItem.Size = new System.Drawing.Size(147, 22);
+            this.cARGARRBDToolStripMenuItem.Text = "CARGAR RND";
+            this.cARGARRBDToolStripMenuItem.Click += new System.EventHandler(this.cARGARRBDToolStripMenuItem_Click);
+            // 
+            // dESCARGARToolStripMenuItem1
+            // 
+            this.dESCARGARToolStripMenuItem1.Name = "dESCARGARToolStripMenuItem1";
+            this.dESCARGARToolStripMenuItem1.Size = new System.Drawing.Size(147, 22);
+            this.dESCARGARToolStripMenuItem1.Text = "DESCARGAR";
+            this.dESCARGARToolStripMenuItem1.Click += new System.EventHandler(this.dESCARGARToolStripMenuItem1_Click);
+            // 
+            // mENU3ToolStripMenuItem
+            // 
+            this.mENU3ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cARGARToolStripMenuItem2,
+            this.cARGARRNDToolStripMenuItem1,
+            this.dESCARGARToolStripMenuItem2});
+            this.mENU3ToolStripMenuItem.Name = "mENU3ToolStripMenuItem";
+            this.mENU3ToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.mENU3ToolStripMenuItem.Text = "MENU 3";
+            // 
+            // cARGARToolStripMenuItem2
+            // 
+            this.cARGARToolStripMenuItem2.Name = "cARGARToolStripMenuItem2";
+            this.cARGARToolStripMenuItem2.Size = new System.Drawing.Size(147, 22);
+            this.cARGARToolStripMenuItem2.Text = "CARGAR";
+            this.cARGARToolStripMenuItem2.Click += new System.EventHandler(this.cARGARToolStripMenuItem2_Click);
+            // 
+            // cARGARRNDToolStripMenuItem1
+            // 
+            this.cARGARRNDToolStripMenuItem1.Name = "cARGARRNDToolStripMenuItem1";
+            this.cARGARRNDToolStripMenuItem1.Size = new System.Drawing.Size(147, 22);
+            this.cARGARRNDToolStripMenuItem1.Text = "CARGAR RND";
+            this.cARGARRNDToolStripMenuItem1.Click += new System.EventHandler(this.cARGARRNDToolStripMenuItem1_Click);
+            // 
+            // dESCARGARToolStripMenuItem2
+            // 
+            this.dESCARGARToolStripMenuItem2.Name = "dESCARGARToolStripMenuItem2";
+            this.dESCARGARToolStripMenuItem2.Size = new System.Drawing.Size(147, 22);
+            this.dESCARGARToolStripMenuItem2.Text = "DESCARGAR";
+            this.dESCARGARToolStripMenuItem2.Click += new System.EventHandler(this.dESCARGARToolStripMenuItem2_Click);
+            // 
+            // eJERCICIO1ToolStripMenuItem
+            // 
+            this.eJERCICIO1ToolStripMenuItem.Name = "eJERCICIO1ToolStripMenuItem";
+            this.eJERCICIO1ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.eJERCICIO1ToolStripMenuItem.Text = "EJERCICIO 1";
+            this.eJERCICIO1ToolStripMenuItem.Click += new System.EventHandler(this.eJERCICIO1ToolStripMenuItem_Click);
+            // 
+            // eJERCICIO2ToolStripMenuItem
+            // 
+            this.eJERCICIO2ToolStripMenuItem.Name = "eJERCICIO2ToolStripMenuItem";
+            this.eJERCICIO2ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.eJERCICIO2ToolStripMenuItem.Text = "EJERCICIO 2";
+            this.eJERCICIO2ToolStripMenuItem.Click += new System.EventHandler(this.eJERCICIO2ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(889, 448);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem opBasicasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mENU1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cARGARToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cARGARRNDToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dESCARGARToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mENU2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cARGARToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem cARGARRBDToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dESCARGARToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem mENU3ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cARGARToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem cARGARRNDToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem dESCARGARToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem eJERCICIO1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eJERCICIO2ToolStripMenuItem;
+    }
+}
+
